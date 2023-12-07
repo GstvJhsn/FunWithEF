7d3261cdf8f205bb94253a3e6248e1b07119bef9
GstvJhsn <147998267+GstvJhsn@users.noreply.github.com>
2023-12-07 17:26:23 +0100

    Setting up the repos
    Adding dependencies and such.

---
+++ FunWithEF/FunWithEF/Data/FishContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FunWithEF.Model;
+
+
+namespace FunWithEF.Data
+{
+    public class FishContext : DbContext
+    {
+        public DbSet<Fish> Shoal { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+    }
+}
---
+++ FunWithEF/FunWithEF/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FunWithEF.Data;
+using FunWithEF.Model;
+
+using (var context = new FishContext())
+{
+    var fish = new Fish { Species = "Perch", Color = "Green" };
+    context.Shoal.Add(fish);
+    context.SaveChanges();
+}
+
